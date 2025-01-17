379b15b8591803e49a01a16e053d7e9c539dcfdf
danielz0102 <danyzglez@hotmail.com>
2025-01-16 19:21:59 -0600

    feat: add navigation dots to slide to a specific image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import '@/index.css'
 import '@/carousel.css'
 
+document.querySelectorAll('.carousel__slide').forEach((slide, i) => {
+  slide.id = `slide${i + 1}`
+
+  const slideBtn = document.createElement('button')
+  slideBtn.classList.add('carousel__nav__btn')
+  slideBtn.dataset.slide = slide.id
+
+  document.querySelector('.carousel__nav').appendChild(slideBtn)
+})
+
 document
   .querySelector('.carousel__btn--left')
   .addEventListener('click', () => previous())
 document
   .querySelector('.carousel__btn--right')
   .addEventListener('click', () => next())
+
+document.querySelectorAll('.carousel__nav__btn').forEach((btn) => {
+  btn.addEventListener('click', () => slideTo(btn.dataset.slide))
+})
 
 function next() {
   const currentSlide = document.querySelector('.carousel__slide:not([hidden])')
@@ -27,5 +41,15 @@ function previous() {
   if (previousElement && isSlide) {
     currentSlide.hidden = true
     previousElement.hidden = false
+  }
+}
+
+function slideTo(slideId) {
+  const newSlide = document.querySelector(`#${slideId}`)
+  const currentSlide = document.querySelector('.carousel__slide:not([hidden])')
+
+  if (newSlide !== currentSlide) {
+    newSlide.hidden = false
+    currentSlide.hidden = true
   }
 }
