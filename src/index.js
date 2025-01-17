87432d8c83125cec00b156c3fc50504ba00f7bb7
danielz0102 <danyzglez@hotmail.com>
2025-01-16 18:43:42 -0600

    feat: create carousel with buttons to slide images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import '@/index.css'
+import '@/carousel.css'
+
+function next() {
+  const currentSlide = document.querySelector('.carousel__slide:not([hidden])')
+  const nextElement = currentSlide?.nextElementSibling
+  const isSlide = nextElement?.classList.contains('carousel__slide')
+
+  if (nextElement && isSlide) {
+    currentSlide.hidden = true
+    nextElement.hidden = false
+  }
+}
+
+function previous() {
+  const currentSlide = document.querySelector('.carousel__slide:not([hidden])')
+  const previousElement = currentSlide?.previousElementSibling
+  const isSlide = previousElement?.classList.contains('carousel__slide')
+
+  if (previousElement && isSlide) {
+    currentSlide.hidden = true
+    previousElement.hidden = false
+  }
+}
+
+document
+  .querySelector('.carousel__btn--left')
+  .addEventListener('click', () => previous())
+document
+  .querySelector('.carousel__btn--right')
+  .addEventListener('click', () => next())
